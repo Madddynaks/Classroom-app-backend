9377d462799e842fba60ecf1f80d98fe341775e7
Madddynaks <madhav.nakra2002@gmail.com>
2024-11-19 17:59:18 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Database Connection
+connectDB();
+
+// Routes
+
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
