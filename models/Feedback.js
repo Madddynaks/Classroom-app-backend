a1cb84677ef91bd24056d010d87184e032fda95b
Madddynaks <madhav.nakra2002@gmail.com>
2024-12-12 10:36:53 +0530

    feedback changes

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const FeedbackSchema = new mongoose.Schema({
-  feedbackID: {type: String, required: true},
   studentID: {type: String, required: true},
   subjectID: {type: String, required: true},
   feedback: {type: String, required: true},
